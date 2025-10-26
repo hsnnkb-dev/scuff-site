f00fb470c072513919f973364d7cceebf8a8dd92
HasanNakib <hasan.nakib@victvs.co.uk>
2025-10-26 13:38:37 +0000

    add redirects file for SPA config

--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import NavigationBar from "../components/NavigationBar";
 
+function VideoSection() {}
+
+function BucketSection() {}
+
 export default function Features() {
   return (
     <>
